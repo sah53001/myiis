8bb05b3f2e5440b423b7cbd7f85d0692ec3f2bef
sah53001 <sheywood@cox.net>
2018-03-28 22:30:51 +0000

    Version 0.2.1: Change to using a template for webpage

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,12 +2,16 @@ powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
 end
 
-file 'C:\inetpub\wwwroot\Default.htm' do
-  content "<h1>Hello, world!</h1>
-  <h2>PLATFORM: #{node['platform']}</h2>
-  <h2>HOSTNAME: #{node['hostname']}</h2>
-  <h2>MEMORY:   #{node['memory']['total']}</h2>
-  <h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
+# file 'C:\inetpub\wwwroot\Default.htm' do
+#   content "<h1>Hello, world!</h1>
+#   <h2>PLATFORM: #{node['platform']}</h2>
+#   <h2>HOSTNAME: #{node['hostname']}</h2>
+#   <h2>MEMORY:   #{node['memory']['total']}</h2>
+#   <h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
+# end
+
+template 'C:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
 
 service 'w3svc' do
